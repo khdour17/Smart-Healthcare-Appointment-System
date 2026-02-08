fbb2d05c80d347da70d0d11ff200529eef106308
Ali Khdour <ali.khdour06@gmail.com>
2026-02-08 12:13:33 +0200

    initial commit

---
+++ src/main/java/org/example/smarthealthcareappointmentsystem/SmartHealthcareAppointmentSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.smarthealthcareappointmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHealthcareAppointmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartHealthcareAppointmentSystemApplication.class, args);
+    }
+
+}
